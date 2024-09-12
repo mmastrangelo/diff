ff596fcae73312f949016f50a8930ce4508bb780
Matt Mastrangelo <mmastrangelo@halix.io>
2024-09-11 23:57:24 -0400

    added diffInterface handling of nil to non-nil slice comparisons

--- diff_interface.go
+++ diff_interface.go
@@ -26,13 +26,49 @@ func (d *Differ) diffInterface(path []string, a, b reflect.Value, parent interfa
 	}
 
 	if a.IsNil() {
-		d.cl.Add(UPDATE, path, nil, exportInterface(b), parent)
-		return nil
+		// if b is a slice of map[string]interface{}, set a to an empty slice of the same type
+		// This allows us to identify a change from a nil slice to a populated slice as create operations
+		// rather than an update of the entire slice
+		aUpdated := false
+		if b.Elem().Kind() == reflect.Slice {
+			if b.Elem().Len() == 0 {
+				return nil
+			} else if b.Elem().Index(0).Elem().Kind() == reflect.Map {
+				var aVal interface{}
+				a = reflect.ValueOf(&aVal).Elem()
+				a.Set(reflect.MakeSlice(b.Elem().Type(), 0, 0))
+
+				aUpdated = true
+			}
+		}
+
+		if !aUpdated {
+			d.cl.Add(UPDATE, path, nil, exportInterface(b), parent)
+			return nil
+		}
 	}
 
 	if b.IsNil() {
-		d.cl.Add(UPDATE, path, exportInterface(a), nil, parent)
-		return nil
+		// if a is a slice of map[string]interface{}, set b to an empty slice of the same type
+		// This allows us to identify a change from a populated slice to a nil slice as delete operations
+		// rather than an update of the entire slice
+		bUpdated := false
+		if a.Elem().Kind() == reflect.Slice {
+			if a.Elem().Len() == 0 {
+				return nil
+			} else if a.Elem().Index(0).Elem().Kind() == reflect.Map {
+				var bVal interface{}
+				b = reflect.ValueOf(&bVal).Elem()
+				b.Set(reflect.MakeSlice(a.Elem().Type(), 0, 0))
+
+				bUpdated = true
+			}
+		}
+
+		if !bUpdated {
+			d.cl.Add(UPDATE, path, exportInterface(a), nil, parent)
+			return nil
+		}
 	}
 
 	return d.diff(path, a.Elem(), b.Elem(), parent)
